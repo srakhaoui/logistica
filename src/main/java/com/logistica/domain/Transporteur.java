eee109eaf77d33a2b8ea28c7b26eb1502c42f185
Said RAKHAOUI <srakhaoui@hotmail.com>
2021-05-23 17:41:57 +0200

    Update BL number's type

--- src/main/java/com/logistica/domain/Transporteur.java
+++ src/main/java/com/logistica/domain/Transporteur.java
@@ -22,8 +22,8 @@ public class Transporteur implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sequenceGenerator")
-    @SequenceGenerator(name = "sequenceGenerator")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "transporteurSequenceGenerator")
+    @SequenceGenerator(name = "transporteurSequenceGenerator", sequenceName = "transporteur-seq", allocationSize = 1)
     private Long id;
 
     @NotNull
