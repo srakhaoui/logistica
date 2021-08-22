bd0f7e91328ecf8710e282dfec0f69ebbaeabfd0
Said RAKHAOUI <srakhaoui@hotmail.com>
2021-08-22 10:14:51 +0200

    SequenceGenerator

--- src/main/java/com/logistica/domain/GasoilVenteGros.java
+++ src/main/java/com/logistica/domain/GasoilVenteGros.java
@@ -22,8 +22,8 @@ public class GasoilVenteGros implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "gasoilVenteGrosSequenceGenerator")
-    @SequenceGenerator(name = "gasoilVenteGrosSequenceGenerator", sequenceName = "gasoil-vente-gros-seq", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sequenceGenerator")
+    @SequenceGenerator(name = "sequenceGenerator")
     private Long id;
 
     @NotNull
