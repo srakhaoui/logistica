a46a66b65570f110a19d52720b38abdd37c7cff7
Said RAKHAOUI <srakhaoui@hotmail.com>
2021-09-25 14:58:25 +0200

    Revert "H2 SequenceGenerator"

--- src/main/java/com/logistica/domain/Client.java
+++ src/main/java/com/logistica/domain/Client.java
@@ -21,8 +21,8 @@ public class Client implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sequenceGenerator")
-    @SequenceGenerator(name = "sequenceGenerator")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "clientSequenceGenerator")
+    @SequenceGenerator(name = "clientSequenceGenerator", sequenceName = "client-seq", allocationSize = 1)
     private Long id;
 
     @NotNull
