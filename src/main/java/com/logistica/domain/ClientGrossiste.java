7329fec30d207c3b21624588ab624b4f35c6b623
Said RAKHAOUI <srakhaoui@hotmail.com>
2022-01-02 18:21:57 +0100

    Revert "H2 SequenceGenerator"

--- src/main/java/com/logistica/domain/ClientGrossiste.java
+++ src/main/java/com/logistica/domain/ClientGrossiste.java
@@ -20,8 +20,8 @@ public class ClientGrossiste implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sequenceGenerator")
-    @SequenceGenerator(name = "sequenceGenerator")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "clientGrossisteSequenceGenerator")
+    @SequenceGenerator(name = "clientGrossisteSequenceGenerator", sequenceName = "client-grossite-seq", allocationSize = 1)
     private Long id;
 
     @NotNull
